7c2ae27a5e2b19a3cdab1e638e0beab355d896b4
dylan <dylankang14@gmail.com>
2024-06-18 21:29:55 +0900

    8.5 Complete session create and login

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,11 +1,40 @@
 "use server";
 
+import bcrypt from "bcrypt";
 import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { getSession } from "@/lib/session";
 
 const checkUsername = (username: string) => !username.includes("potato");
 const checkPassword = ({ password, confirmPassword }: { password: string; confirmPassword: string }) =>
 	password === confirmPassword;
+
+const checkUniqueUsername = async (username: string) => {
+	const user = await db.user.findUnique({
+		where: {
+			username,
+		},
+		select: {
+			id: true,
+		},
+	});
+	return !Boolean(user);
+};
+const checkUniqueEmail = async (email: string) => {
+	const user = await db.user.findUnique({
+		where: {
+			email,
+		},
+		select: {
+			id: true,
+		},
+	});
+	return !Boolean(user);
+};
 
 const formSchema = z
 	.object({
@@ -17,45 +46,61 @@ const formSchema = z
 			.min(3, "Username is too short.")
 			.max(10, "Username is too long.")
 			.trim()
-			.refine(checkUsername, "Not allowed 'potato' in username."),
+			.refine(checkUsername, "Not allowed 'potato' in username.")
+			.refine(checkUniqueUsername, "This username is already taken"),
 		email: z
 			.string({
 				invalid_type_error: "email must be string.",
 				required_error: "email required.",
 			})
 			.email()
-			.toLowerCase(),
+			.toLowerCase()
+			.refine(checkUniqueEmail, "This Email is already taken"),
 		password: z
 			.string({
 				invalid_type_error: "password must be string.",
 				required_error: "password required.",
 			})
-			.min(PASSWORD_MIN_LENGTH, "Password must be 4 more characters.")
-			.regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+			.min(PASSWORD_MIN_LENGTH, "Password must be 4 more characters."),
+		// .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
 		confirmPassword: z
 			.string({
 				invalid_type_error: "confirmPassword must be string.",
 				required_error: "confirmPassword required.",
 			})
-			.min(PASSWORD_MIN_LENGTH, "Password must be 4 more characters.")
-			.regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+			.min(PASSWORD_MIN_LENGTH, "Password must be 4 more characters."),
+		// .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
 	})
 	.refine(checkPassword, {
 		message: "Password and Confirm password must be the same.",
 		path: ["confirmPassword"],
 	});
 
-export const createAccount = (prevState: any, formData: FormData) => {
+export const createAccount = async (prevState: any, formData: FormData) => {
 	const data = {
 		username: formData.get("username"),
 		email: formData.get("email"),
 		password: formData.get("password"),
 		confirmPassword: formData.get("confirmPassword"),
 	};
-	const result = formSchema.safeParse(data);
+	const result = await formSchema.spa(data);
 	if (!result.success) {
 		return result.error.flatten();
 	} else {
-		console.log(result.data);
+		const hashedPassword = await bcrypt.hash(result.data.password, 12);
+		const user = await db.user.create({
+			data: {
+				username: result.data.username,
+				email: result.data.email,
+				password: hashedPassword,
+			},
+			select: {
+				id: true,
+			},
+		});
+		const session = await getSession();
+		session.id = user.id;
+		await session.save();
+		redirect("/profile");
 	}
 };
--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,23 +1,61 @@
 "use server";
 
 import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import { getSession } from "@/lib/session";
+import { redirect } from "next/navigation";
 
+const checkEmailExists = async (email: string) => {
+	const user = await db.user.findUnique({
+		where: {
+			email,
+		},
+		select: {
+			id: true,
+		},
+	});
+	return Boolean(user);
+};
 const loginSchema = z.object({
-	email: z.string().email().toLowerCase(),
+	email: z.string().email().toLowerCase().refine(checkEmailExists, "Account email not exists."),
 	password: z
 		.string({
 			required_error: "Password is required.",
 		})
-		.min(PASSWORD_MIN_LENGTH)
-		.regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+		.min(PASSWORD_MIN_LENGTH),
+
+	// .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
 });
 export const login = async (prevState: any, formData: FormData) => {
 	const data = Object.fromEntries(formData.entries());
-	const result = loginSchema.safeParse(data);
+	const result = await loginSchema.spa(data);
 	if (!result.success) {
 		return result.error.flatten();
 	} else {
-		console.log(result.data);
+		const user = await db.user.findUnique({
+			where: {
+				email: result.data.email,
+			},
+			select: {
+				id: true,
+				password: true,
+			},
+		});
+		const ok = await bcrypt.compare(result.data.password, user!.password ?? "");
+		if (ok) {
+			const session = await getSession();
+			session.id = user!.id;
+			await session.save();
+			redirect("/profile");
+		} else {
+			return {
+				fieldErrors: {
+					password: ["Wrong password."],
+					email: [],
+				},
+			};
+		}
 	}
 };
